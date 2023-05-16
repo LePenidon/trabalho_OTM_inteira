27cb4f41acd17d8991060b2b55cb4ba87564238e
lepenidon <penido1000@gmail.com>
2023-05-16 09:24:09 -0300

    ?

--- etapa_02/code/instancias/arrumar.py
+++ etapa_02/code/instancias/arrumar.py
@@ -1,11 +1,17 @@
+import sys
+
+
 def remove_first_end_spaces(string):
     return "".join(string.rstrip().lstrip())
 
 
-file1 = open('5.txt', 'r')
+instancia = int(sys.argv[1])
+
+
+file1 = open(str(instancia)+'.txt', 'r')
 Lines = file1.readlines()
 
-file2 = open('5_bom.txt', 'w')
+file2 = open(str(instancia)+'_bom'+'.txt', 'w')
 count = 0
 for line in Lines:
     count += 1
